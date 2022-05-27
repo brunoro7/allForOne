14fce473dd426577e392fe9d2582405a42b4078a
brunoro7 <bruno.ro7c@gmail.com>
2022-05-27 03:41:47 -0300

    fix: 'correção da descrição do fix no commit." feat: desafio11 -Exibir os dados da coluna 'notes' da tabela 'purchase_orders' em que seu valor de 'Purchase generated based on Order' é maior ou igual a 30 e menor ou igual a 39;

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,4 @@
 SELECT notes
 FROM northwind.purchase_orders
 WHERE notes >= 'Purchase generated based on Order #30'
-AND notes <= 'Purchase generated based on Order #39'
-;
+AND notes <= 'Purchase generated based on Order #39';
